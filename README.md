8f83dabc6fb60503f630256bb90db2d6a0082ed1
Erik Weber <terbolous@gmail.com>
2015-10-06 12:41:39 +0200

    Updated readme and mgmt/init2.sh

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Docker Compose files to run a full Apache CloudStack environment
 # Requirements
  - A recent kernel (I prefer 3.19 series on Ubuntu 14.04)
  - Port 22 should be available on the host, for the KVM container, which means you'll have to set up your ssh on another port
+
+# Docker configuration
+Docker should be manually configured with a subnet range, in particular to allow configuration of the network.
+
+For ubuntu, add the following to `/etc/default/docker`:
+`DOCKER_OPTS="--bip=192.168.100.1/24 --fixed-cidr=192.168.100.0/28"`
+
--- mgmt/init2.sh
+++ mgmt/init2.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
 
-#yum install -y nfs-utils portmap rpcbind
-#service portmap start
-yum install -y portmap
-
+# Configure NFS
+yum install -y portmap nfs-utils openssh-server
+echo "root:password" | chpasswd
+sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 cat <<EOF > /etc/exports
-/mgmt/exports  *(rw,async,no_root_squash,no_subtree_check)
+/exports  *(rw,async,no_root_squash,no_subtree_check,fsid=0)
+/mgmt *(rw,async,no_root_squash,no_subtree_check,fsid=0)
 EOF
 
-service portmap start
+#Start NFS services
+service rpcbind start
 service nfs start
+service nfs restart
+service sshd start
+
+exportfs -a
+
+# Wait for MySQL server
+sleep 10
+
+# Install systemvm template
+#if [ ! -f /mgmt/exports/secondary/template/tmpl/1/3/template.properties ]; then
+#    /usr/share/cloudstack-common/scripts/storage/secondary/cloud-install-sys-tmplt \ 
+#    -m /mgmt/exports/secondary \ 
+#    -h kvm \ 
+#    -F \ 
+#    -u http://jenkins.buildacloud.org/job/build-systemvm64-master/lastSuccessfulBuild/artifact/tools/appliance/dist/systemvm64template-master-4.6.0-kvm.qcow2.bz2 \ 
+#    -o ${MYSQL_PORT_3306_TCP_ADDR}
+#fi
+
+
+
+# Ugly hack to get api.integration.port configured
+#grep 'integration.api.port' /root/init.sh
+#if [ $? = 1 ]; then
+#    cat <<EOF >/root/add.txt
+#mysql -hdatabase -uroot -p${MYSQL_ENV_MYSQL_ROOT_PASSWORD} -e "UPDATE cloud.configuration SET value='8096' WHERE name='integration.api.port'"
+#EOF
+#sed -i '/esac/r /root/add.txt' /root/init.sh
+#fi
+# Continue with regular mgmt server startup
+/root/init.sh
+
