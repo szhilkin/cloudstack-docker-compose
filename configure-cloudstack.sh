1d1442d11bcc0c9ad8c53b71fb14ab70d6f82b71
Xavier Geerinck <thebillkidy@gmail.com>
2016-02-23 11:43:08 +0100

    Cloudstack agent running on docker

---
+++ configure-cloudstack.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+#################################################################################
+#
+#          FILE:  configure.sh
+#         USAGE:  ./configure.sh
+#   DESCRIPTION: This script will configure the entire cloudstack environment
+#                as soon as the docker-compose up script has finished booting
+#
+#       OPTIONS:  
+#
+#  REQUIREMENTS:  docker-compose up should be finished
+#        AUTHOR:  Xavier Geerinck (xavier.geerinck@gmail.com)
+#       COMPANY:  /
+#       VERSION:  1.0.0
+#       CREATED:  17/FEB/16 10:36 CET
+#      REVISION:  1.0 - Base POC script
+#
+##################################################################################
+##                            Environment Variables                             ##
+##################################################################################
+dns_ext=172.31.221.250
+dns_int=172.31.221.250
+nmask=255.255.255.0
+hpvr=KVM
+pod_gw=172.17.0.1
+pod_start=172.17.0.220
+pod_end=172.17.0.254
+vlan_gw=10.30.28.1
+vlan_start=10.30.28.220
+vlan_end=10.30.28.254
+ 
+#Put space separated host ips in following
+host_ips=10.30.28.4
+host_user=root
+host_passwd=password
+sec_storage=nfs://10.30.28.4/root/docker-compose-test/mgmt/exports/secondary
+prm_storage=nfs://10.30.28.4/root/docker-compose-test/mgmt/exports/primary
+ 
+##################################################################################
+##                                   Utils                                      ##
+##################################################################################
+function execute_cmd {
+    docker exec cloudmonkey /bin/bash -c "cloudmonkey -p local -c config $@"
+}
+
+function print_green {
+    echo -e "\e[92m$@\e[39m"
+}
+
+function print_blue {
+    echo -e "\e[34m$@\e[39m"
+}
+
+function print_red {
+    echo -e "\e[31m$@\e[39m"
+}
+
+function print_yellow {
+    echo -e "\e[33m$@\e[39m"
+}
+
+function print_title {
+    print_blue "#########################################################"
+    print_blue "$@"
+    print_blue "#########################################################"
+}
+
+function print_error {
+    print_red "[ERROR] $@" 1>&2
+    exit 1
+}
+
+# $1 = file
+# $2 = key
+# $3 = new value
+function set_config {
+    if [ -z $1 -o -z $1 ]; then
+        return;
+    fi
+
+    sed -i "s/^\($2\s*=\s*\).*\$/\2$3/" $1
+}
+
+##################################################################################
+##                                   Script                                     ##
+##################################################################################
+# Make sure that the cloudmonkey container exists
+if ! docker ps | grep cloudmonkey >/dev/null; then
+    print_error "[Cloudmonkey] No container named cloudmonkey";
+fi
+
+execute_cmd "list accounts"
+
+print_green "[Cloudmonkey] Creating zone" $zone_id
+zone_id=`execute_cmd "create zone dns1=$dns_ext internaldns1=$dns_int name=DemoZone2 networktype=Basic" | grep ^id\ = | awk '{print $3}'`
+ 
+print_green "[Cloudmonkey] Creating physical network" $phy_id
+phy_id=`execute_cmd "create physicalnetwork name=phy-network zoneid=$zone_id" | grep ^id\ = | awk '{print $3}'`
+
+print_green "[Cloudmonkey] Adding guest traffic"
+# kvmnetworklabel=cloudbr0
+execute_cmd "add traffictype traffictype=Guest physicalnetworkid=$phy_id"
+print_green "[Cloudmonkey] Adding mgmt traffic"
+execute_cmd "add traffictype traffictype=Management physicalnetworkid=$phy_id"
+print_green "[Cloudmonkey] Enabling physicalnetwork"
+execute_cmd "update physicalnetwork state=Enabled id=$phy_id"
+ 
+print_green "[Cloudmonkey] Enabling virtual router element and network service provider"
+nsp_id=`execute_cmd "list networkserviceproviders name=VirtualRouter physicalnetworkid=$phy_id" | grep ^id\ = | awk '{print $3}'`
+vre_id=`execute_cmd "list virtualrouterelements nspid=$nsp_id" | grep ^id\ = | awk '{print $3}'`
+execute_cmd "api configureVirtualRouterElement Enabled=true id=$vre_id"
+execute_cmd "update networkserviceprovider state=Enabled id=$nsp_id"
+ 
+print_green "[Cloudmonkey] Enabling security group provider"
+nsp_sg_id=`execute_cmd "list networkserviceproviders name=SecurityGroupProvider physicalnetworkid=$phy_id" | grep ^id\ = | awk '{print $3}'`
+execute_cmd "update networkserviceprovider state=Enabled id=$nsp_sg_id"
+ 
+print_green "[Cloudmonkey] Creating network $net_id for zone" $zone_id
+netoff_id=`execute_cmd "list networkofferings name=DefaultSharedNetworkOfferingWithSGService" | grep ^id\ = | awk '{print $3}'`
+net_id=`execute_cmd "create network zoneid=$zone_id name=guestNetworkForBasicZone displaytext=guestNetworkForBasicZone networkofferingid=$netoff_id" | grep ^id\ = | awk '{print $3}'`
+ 
+print_green "[Cloudmonkey] Creating pod"
+pod_id=`execute_cmd "create pod name=DemoPod zoneid=$zone_id gateway=$pod_gw netmask=$nmask startip=$pod_start endip=$pod_end" | grep ^id\ = | awk '{print $3}'`
+ 
+print_green "[Cloudmonkey] Creating IP ranges for instances"
+execute_cmd "create vlaniprange podid=$pod_id networkid=$net_id gateway=$vlan_gw netmask=$nmask startip=$vlan_start endip=$vlan_end forvirtualnetwork=false"
+ 
+print_green "[Cloudmonkey] Creating cluster" $cluster_id
+cluster_id=`execute_cmd "add cluster zoneid=$zone_id hypervisor=$hpvr clustertype=CloudManaged podid=$pod_id clustername=DemoCluster" | grep ^id\ = | awk '{print $3}'`
+ 
+# Fix the agent.properties on the KVM
+#docker exec cloudstack-kvm /bin/bash -c "sudo sed -i \"s/^\(private.network.device\s*=\s*\).*\$/\1docker0/\" /etc/cloudstack/agent/agent.properties"
+
+#Put loop here if more than one
+for host_ip in $host_ips;
+do
+  print_green "[Cloudmonkey] Adding host" $host_ip;
+  execute_cmd "add host zoneid=$zone_id podid=$pod_id clusterid=$cluster_id hypervisor=$hpvr username=$host_user password=$host_passwd url=http://$host_ip";
+done;
+#service nfs restart 
+print_green "[Cloudmonkey] Adding primary storage"
+execute_cmd "create storagepool zoneid=$zone_id podid=$pod_id clusterid=$cluster_id name=DemoPrimary url=$prm_storage"
+ 
+print_green "[Cloudmonkey] Adding secondary storage"
+execute_cmd "add secondarystorage zoneid=$zone_id url=$sec_storage"
+ 
+# Done creating
+execute_cmd "update zone allocationstate=Enabled id=$zone_id"
+print_green "[Cloudmonkey] Basic zone deloyment completed!"
+
+# Poll every 10 seconds until the systemvms are up
+arr_vms=(`execute_cmd "list systemvms state=Running" | grep "^name =" | cut -d ' ' -f 3`)
+
+while [ ${#arr_vms[@]} -eq 0 ]; do
+    sleep 10
+    arr_vms=(`execute_cmd "list systemvms state=Running" | grep "^name =" | cut -d ' ' -f 3`)
+    print_yellow "[Cloudmonkey] Waiting till the systemvms are up"
+done
--- configure.sh
+++ configure.sh
@@ -1,63 +1,243 @@
 #!/bin/bash
+#################################################################################
+#
+#          FILE:  configure.sh
+#         USAGE:  ./configure.sh
+#   DESCRIPTION: This script will configure the entire cloudstack environment
+#                as soon as the docker-compose up script has finished booting
+#
+#       OPTIONS:  
+#
+#  REQUIREMENTS:  docker-compose up should be finished
+#        AUTHOR:  Xavier Geerinck (xavier.geerinck@gmail.com)
+#       COMPANY:  /
+#       VERSION:  1.0.0
+#       CREATED:  17/FEB/16 10:36 CET
+#      REVISION:  1.0 - Base POC script
+#
+#################################################################################
+# Steps: 
+#################################################################################
+# 
+#  1. (Hypervisor) NFS Server
+#      1.1. Install nfs if not installed
+#      1.2. Configure ports
+#      1.3. Configure IPTables
+#      1.4. Exports + Save them
+#      1.5. Restart NFS
+#  2. (KVM) Fix CGROUPS
+#      2.1. Change cgconfig.conf
+#  3. (KVM) SSHD
+#      3.1. Start SSHD
+#  4. (Hypervisor) Configure the cloudstack environment with cloudmonkey
+#      4.1. Install + link docker containers
+#           docker run -it --rm --link cloudstack-mgmt:8080 cloudstack/cloudmonkey
+#      4.2. Run configure.sh
+#  5. (Hypervisor) Network Configuration
+#      5.1. Add cloudbr0 interface
+#      5.2. Create docker0.100 VLAN
+#      5.3. Add docker0.100 vlan to cloudbr0
+#      5.4. Add VNET1 (eth1 SSVM) and VNET4 (eth1 CPVM) to docker0 bridge
+#  6. Done
+#
+##################################################################################
+##                                   Utils                                      ##
+##################################################################################
 
-# FIXME: Check if configuration is done
-# FIXME: get NFS addresses
-NFS_SERVER=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' cloudstack-mgmt)
+function print_green {
+    echo -e "\e[92m$@\e[39m"
+}
 
-set -e
+function print_blue {
+    echo -e "\e[34m$@\e[39m"
+}
 
+function print_red {
+    echo -e "\e[31m$@\e[39m"
+}
 
-EXEC="cloudmonkey -p local -c cloudmonkey.config"
+function print_title {
+    print_blue "#########################################################"
+    print_blue "$@"
+    print_blue "#########################################################"
+}
 
-# Create the Zone
-echo "Creating Zone"
-DUMMY=$($EXEC create zone name="CloudStack Docker Demo - Basic" networktype=Basic dns1=8.8.8.8 internaldns1=8.8.8.8)
-ZONE_ID=$($EXEC list zones filter=id | grep id | cut -f 3 -d ' ')
+function print_error {
+    print_red "[ERROR] $@" 1>&2
+    exit
+}
 
-echo "Creating Physical network"
-# Create and configure physical network
-PHY_ID=$($EXEC create physicalnetwork name=phy-network zoneid=$ZONE_ID | grep ^id\ = | cut -f 3 -d ' ')
+##################################################################################
+##                                   Step 1                                     ##
+##################################################################################
+print_title '[NFS] (Hypervisor) Getting the NFS server ready'
 
-DUMMY=$($EXEC add traffictype traffictype=Guest physicalnetworkid=$PHY_ID)
-DUMMY=$($EXEC add traffictype traffictype=Management physicalnetworkid=$PHY_ID)
-DUMMY=$($EXEC update physicalnetwork state=Enabled id=$PHY_ID)
+# Install NFS if not installed
+print_green '[NFS] Installing NFS if it is not installed'
 
-echo "Configuring Virtual Router"
-nsp_id=`$EXEC list networkserviceproviders name=VirtualRouter physicalnetworkid=$PHY_ID | grep ^id\ = | awk '{print $3}'`
-vre_id=`$EXEC list virtualrouterelements nspid=$nsp_id | grep ^id\ = | awk '{print $3}'`
-DUMMY=$($EXEC api configureVirtualRouterElement enabled=true id=$vre_id)
-DUMMY=$($EXEC update networkserviceprovider state=Enabled id=$nsp_id)
+if ! yum list installed "nfs-utils" >/dev/null 2>&1; then 
+    yum install -y nfs-utils
+fi
 
-echo "Enabling Security Groups"
-nsp_sg_id=`$EXEC list networkserviceproviders name=SecurityGroupProvider physicalnetworkid=$PHY_ID | grep ^id\ = | awk '{print $3}'`
-DUMMY=$($EXEC update networkserviceprovider state=Enabled id=$nsp_sg_id)
+if ! yum list installed "nfs-utils-lib" >/dev/null 2>&1; then
+    yum install -y nfs-utils-lib
+fi
 
-echo "Creating default network"
-netoff_id=`$EXEC list networkofferings name=DefaultSharedNetworkOfferingWithSGService | grep ^id\ = | awk '{print $3}'`
-net_id=`$EXEC create network zoneid=$ZONE_ID name=guestNetworkForBasicZone displaytext=guestNetworkForBasicZone networkofferingid=$netoff_id | grep ^id\ = | awk '{print $3}'`
+# Configure ports
+print_green '[NFS] Configuring the NFS ports'
 
-echo "Creating Pod"
-pod_id=`$EXEC create pod name=DemoPod zoneid=$ZONE_ID gateway=192.168.100.1 netmask=255.255.255.0 startip=192.168.100.20 endip=192.168.100.25 | grep ^id\ = | awk '{print $3}'`
+cat <<EOF > /etc/sysconfig/nfs
+LOCKD_TCPPORT=32803
+LOCKD_UDPPORT=32769
+MOUNTD_PORT=892
+NEED_STATD=yes
+STATD_PORT=662
+STATD_OUTGOING_PORT=2020
+GSS_USE_PROXY="yes"
+RPCNFSDARGS="-N 4 -N 4.1 -N 4.2"
+EOF
 
-echo "Configuring Guest IP range"
-DUMMY=$($EXEC create vlaniprange podid=$pod_id networkid=$net_id gateway=192.168.100.1 netmask=255.255.255.0 startip=192.168.100.30 endip=192.168.100.50 forvirtualnetwork=false)
+# Configure IPTables
+print_green '[NFS] Configuring IPTables to allow incoming traffic'
 
-echo "Creating cluster"
-cluster_id=`$EXEC add cluster zoneid=$ZONE_ID hypervisor=KVM clustertype=CloudManaged podid=$pod_id clustername=DemoCluster | grep ^id\ = | awk '{print $3}'`
+# See: https://www.centos.org/docs/5/html/5.2/Deployment_Guide/s2-sysconfig-nfs.html
+iptables -A INPUT -m state --state NEW -p udp --dport 111   -j ACCEPT # Portmap/sunrpc
+iptables -A INPUT -m state --state NEW -p tcp --dport 111   -j ACCEPT # Portmap/sunrpc
+iptables -A INPUT -m state --state NEW -p udp --dport 2049  -j ACCEPT # Main Port
+iptables -A INPUT -m state --state NEW -p tcp --dport 2049  -j ACCEPT # Main Port
+iptables -A INPUT -m state --state NEW -p tcp --dport 32803 -j ACCEPT # LOCKD_TCPPORT
+iptables -A INPUT -m state --state NEW -p udp --dport 32769 -j ACCEPT # LOCKD_UDPPORT
+iptables -A INPUT -m state --state NEW -p tcp --dport 892   -j ACCEPT # Mountd
+iptables -A INPUT -m state --state NEW -p udp --dport 892   -j ACCEPT # Mountd
+iptables -A INPUT -m state --state NEW -p tcp --dport 875   -j ACCEPT 
+iptables -A INPUT -m state --state NEW -p udp --dport 875   -j ACCEPT
+iptables -A INPUT -m state --state NEW -p tcp --dport 662   -j ACCEPT # Statd
+iptables -A INPUT -m state --state NEW -p udp --dport 662   -j ACCEPT # Statd
 
-echo "Adding host"
-DUMMY=$($EXEC add host zoneid=$ZONE_ID podid=$pod_id clusterid=$cluster_id hypervisor=KVM username=root password=password url=http://192.168.100.1)
+# Exports + Save
+print_green '[NFS] Exporting the directories for cloudstack'
 
-echo "Creating primary storage"
-DUMMY=$($EXEC create storagepool zoneid=$ZONE_ID podid=$pod_id clusterid=$cluster_id name=DemoPrimary provider=DefaultPrimary scope=cluster url=nfs://${NFS_SERVER}/exports/primary)
+# Set chmod 777 on exported dir, http://mail-archives.apache.org/mod_mbox/cloudstack-users/201505.mbox/%3CCAHed4gZJqSkZW-gRGUudPP1tS7_XvBxcD70iz9qAMNRET8s0Zw@mail.gmail.com%3E
+chmod -R 777 /root/docker-compose-test/mgmt
 
-echo "Creating secondary storage"
-DUMMY=$($EXEC add imagestore name=DemoSecondary provider=NFS zoneid=$ZONE_ID url=nfs://${NFS_SERVER}/exports/secondary)
+# Create exports
+cat <<EOF > /etc/exports
+/root/docker-compose-test/mgmt *(rw,async,no_root_squash,no_subtree_check,fsid=0,insecure)
+EOF
 
+print_green '[NFS] Saving the exports'
 
+exportfs -rav # Save exports
 
-echo "Enabling Zone"
-$EXEC update zone allocationstate=Enabled id=$ZONE_ID
+# Restart NFS
+print_green '[NFS] Restarting NFS'
+service nfslock stop
+service nfs stop
+service rpc-statd stop
+service rpcbind stop
+umount /proc/fs/nfsd
+service rpcbind start
+service rpc-statd start
+service nfs start
+service nfslock start
 
-echo "All done!"
+##################################################################################
+##                                   Step 2                                     ##
+##################################################################################
+print_title '[CGROUPS] (KVM) Fixing CGROUPS'
 
+# Changing the cgconfig.conf file
+print_green '[CGROUPS] Fixing the cgconfig.conf file'
+
+docker exec cloudstack-kvm /bin/bash -c "cat <<EOF > /etc/cgconfig.conf
+mount {
+        cpuset          = /cgroup/cpuset;
+        cpu,cpuacct     = /cgroup/cpu,cpuacct;
+        memory          = /cgroup/memory;
+        devices         = /cgroup/devices;
+        freezer         = /cgroup/freezer;
+        net_cls         = /cgroup/net_cls;
+        blkio           = /cgroup/blkio;
+}
+EOF"
+##################################################################################
+##                                   Step 3                                     ##
+##################################################################################
+print_title '[SSHD] (KVM) Starting'
+print_green '[SSHD] Starting SSHD so that we can use it as a host'
+docker exec cloudstack-kvm /bin/bash -c "service sshd start"
+
+##################################################################################
+##                                   Step 4                                     ##
+##################################################################################
+print_title '[Cloudmonkey] (Hypervisor) Configuring the cloudstack environment'
+
+# First configure the bridges
+################################################################################
+# add cloudbr0 interface
+print_green '[Network] Adding cloudbr0 bridge'
+
+if ! brctl show | grep -e "^cloudbr0" >/dev/null; then
+    brctl addbr cloudbr0
+fi
+
+# Add docker0.100 VLAN
+if ! brctl show | grep -e "^docker0" >/dev/null; then
+    print_error 'docker0 bridge does not exist, exiting'
+fi
+
+print_green '[Network] Adding the docker0.100 VLAN'
+
+if ! ip link | grep docker0.100@ >/dev/null; then
+    ip link add link docker0 name docker0.100 type vlan id 100
+fi
+
+# Add docker0.100 VLAN to the cloudbr0 bridge
+print_green '[Network] Adding docker0.100 VLAN to the cloudbr0 bridge'
+
+# Add docker0.100 VLAN to the cloudbr0 bridge
+print_green '[Network] Adding docker0.100 VLAN to the cloudbr0 bridge'
+
+if ! brctl show cloudbr0 | grep docker0.100 >/dev/null; then
+    brctl addif cloudbr0 docker0.100
+fi
+
+# Now the cloudmonkey setup
+##################################################################################
+print_green '[Cloudmonkey] Installing the cloudmonkey container and running our configure'
+docker run -itd -v `pwd`:/cloudmonkey --name cloudmonkey cloudstack/cloudstack-cloudmonkey
+./configure-cloudstack.sh
+docker stop cloudmonkey
+#docker rm cloudmonkey
+
+# Cleanup cloudmonkey
+docker rm -f $(docker ps -a | grep cloudstack/cloudstack-cloudmonkey | awk "{print \$1}")
+
+##################################################################################
+##                                   Step 5                                     ##
+##################################################################################
+print_title '[Network] (Hypervisor) Configuring the network'
+print_green '[Network] Removing unneeded lines from iptables'
+
+# Drop specific rules from the iptables output
+for i in $(iptables -L FORWARD --line-numbers | grep -E "DROP\s*all" | awk '{ print $1 }'); do 
+    iptables -D FORWARD $i
+done
+
+# TODO: Needs to sleep here, we need to wait till the agent caught up and created the system vm's
+# TODO: So this will need to move to it's own step
+# Add vnet1 (eth1 SSVM) and vnet4 (eth1 CPVM) to docker0 bridge
+print_green '[Network] Adding vnet1 and vnet4 to the docker0 bridge'
+
+if ! brctl show docker0 | grep vnet1; then 
+    brctl delif cloudbr0 vnet1
+    brctl addif docker0 vnet1
+fi
+
+if ! brctl show docker0 | grep vnet4; then
+    brctl delif cloudbr0 vnet4
+    brctl addif docker0 vnet4
+fi
+
+ifconfig cloudbr0 10.30.28.1 netmask 255.255.255.0
+ifconfig cloudbr0 up
